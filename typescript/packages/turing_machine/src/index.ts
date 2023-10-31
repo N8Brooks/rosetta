8094f9a778d216b04057bac8012b59d2ea4afb21
skookumchoocher <natebrooks004@gmail.com>
2023-10-31 14:45:47 -0500

    refactor: update turing machine

--- typescript/packages/turing_machine/src/index.ts
+++ typescript/packages/turing_machine/src/index.ts
@@ -1,48 +1,88 @@
-/** M configuration or Turing State where `"q0"` is start and `"qf"` is finish */
-export type State = "q0" | "qf" | any;
-
-/** Symbol scanned where `undefined` is blank */
-export type TapeSymbol = undefined | any;
-
-/** An instruction for processing */
+/** A 5-tuple instruction for processing */
 export type Rule = {
-  /** The matching state for the rule */
-  currentState: State;
-  /** The matching tape symbol for the rule */
-  tapeSymbol: TapeSymbol;
-  /** The print operation, where not printing is done by printing the current symbol and "erase" erases the symbol */
-  printOperation: "erase" | TapeSymbol;
+  /** Matching m configuration or Turing State */
+  matchState: string;
+
+  /** The matching tape symbol for the rule where `"blank"` is blank */
+  matchSymbol: string;
+
+  /** The print operation, where `"none"` doesn't change the current cell and `"erase"` erases the symbol */
+  print: string;
+
   /** Move to the left with `-1`, stay (or noop) with `0`, and right with `1`. */
-  tapeMotion: number;
-  /** The resulting state */
-  finalState: State;
+  motion: number;
+
+  /** The resulting m-configuration or turing state where `"finish"` ends */
+  state: string;
 };
 
-export function turingMachine(
-  state: State,
-  rules: readonly Rule[],
-  tape: TapeSymbol[],
-  maxOperations = Infinity
-): TapeSymbol[] {
-  /** The initial tape, stored for erase operations */
-  const tape0 = [...tape] as const;
-  /** A model where the head moves and the tape is stationary */
-  let head = 0;
+export class TuringMachine {
+  #rules: readonly Rule[];
+  #state0: string;
 
-  for (let i = 0; i < maxOperations && state !== "qf"; i++) {
-    const symbol = tape[head];
-
-    const { printOperation, tapeMotion, finalState } = rules.find(
-      ({ currentState, tapeSymbol }) =>
-        state === currentState && tapeSymbol === symbol
-    )!;
-
-    tape[head] = printOperation === "erase" ? tape0[head] : printOperation;
-
-    state = finalState;
-
-    head += tapeMotion;
+  constructor(state0: string, rules: Rule[]) {
+    this.#rules = [...rules];
+    this.#state0 = state0;
   }
 
-  return tape;
+  perform(_tape0: string[], maxOperations = Infinity) {
+    let state = this.#state0;
+
+    const tape = new Tape(_tape0);
+    const tape0 = new Tape(_tape0);
+
+    for (let i = 0; i < maxOperations && state !== "finish"; i++) {
+      const rule = this.#findRule(state, tape.head);
+
+      if (rule.print !== "none") {
+        tape.head = rule.print === "erase" ? tape0.head : rule.print;
+      }
+
+      state = rule.state;
+
+      tape0.move(rule.motion);
+      tape.move(rule.motion);
+    }
+
+    return tape.toArray();
+  }
+
+  #findRule(state: string, symbol: string) {
+    return this.#rules.find(
+      ({ matchState: currentState, matchSymbol: tapeSymbol }) =>
+        state === currentState && symbol == tapeSymbol
+    )!;
+  }
+}
+
+class Tape {
+  /** left side of the tape */
+  #leftSide: string[] = [];
+  /** Current symbol */
+  head: string;
+  /** Reversed right side of the tape */
+  #rightSide: string[];
+
+  constructor(rightSide: string[]) {
+    [this.head = "blank", ...this.#rightSide] = rightSide;
+  }
+
+  move(direction: number) {
+    switch (Math.sign(direction)) {
+      case -1:
+        this.#rightSide.push(this.head);
+        this.head = this.#leftSide.pop() ?? "blank";
+        break;
+      case 0:
+        break;
+      case 1:
+        this.#leftSide.push(this.head);
+        this.head = this.#rightSide.pop() ?? "blank";
+        break;
+    }
+  }
+
+  toArray() {
+    return [...this.#leftSide, this.head, ...this.#rightSide.toReversed()];
+  }
 }
--- typescript/packages/turing_machine/test/index.test.ts
+++ typescript/packages/turing_machine/test/index.test.ts
@@ -1,76 +1,80 @@
 import { describe, expect, it } from "@jest/globals";
-import { Rule, TapeSymbol, turingMachine } from "../src";
-
-describe("Increment unary number", () => {
-  const incrementUnaryNumber: Rule[] = [
-    {
-      currentState: "q0",
-      tapeSymbol: 1,
-      printOperation: 1,
-      tapeMotion: 1,
-      finalState: "q0",
-    },
-    {
-      currentState: "q0",
-      tapeSymbol: undefined,
-      printOperation: 1,
-      tapeMotion: 1,
-      finalState: "qf",
-    },
-  ];
-
-  it("0 -> 1", () => {
-    const tape0: TapeSymbol[] = [];
-    const actual = turingMachine("q0", incrementUnaryNumber, tape0);
-    expect(actual).toEqual([1]);
-  });
-
-  it("3 -> 4", () => {
-    const tape0 = [1, 1, 1];
-    const actual = turingMachine("q0", incrementUnaryNumber, tape0);
-    expect(actual).toEqual([1, 1, 1, 1]);
-  });
-});
+import { TuringMachine } from "../src";
 
 describe("Turing's very first example", () => {
-  const zeroOneSequency: Rule[] = [
+  const zeroOneSequence = new TuringMachine("b", [
     {
-      currentState: "b",
-      tapeSymbol: undefined,
-      printOperation: 0,
-      tapeMotion: 1,
-      finalState: "c",
+      matchState: "b",
+      matchSymbol: "blank",
+      print: "0",
+      motion: 1,
+      state: "c",
     },
     {
-      currentState: "c",
-      tapeSymbol: undefined,
-      printOperation: undefined,
-      tapeMotion: 1,
-      finalState: "d",
+      matchState: "c",
+      matchSymbol: "blank",
+      print: "blank",
+      motion: 1,
+      state: "d",
     },
     {
-      currentState: "d",
-      tapeSymbol: undefined,
-      printOperation: 1,
-      tapeMotion: 1,
-      finalState: "e",
+      matchState: "d",
+      matchSymbol: "blank",
+      print: "1",
+      motion: 1,
+      state: "e",
     },
     {
-      currentState: "e",
-      tapeSymbol: undefined,
-      printOperation: undefined,
-      tapeMotion: 1,
-      finalState: "b",
+      matchState: "e",
+      matchSymbol: "blank",
+      print: "none",
+      motion: 1,
+      state: "b",
     },
-  ];
+  ]);
 
   it("4 sequence", () => {
-    const actual = turingMachine("b", zeroOneSequency, [], 4);
-    expect(actual).toEqual([0, undefined, 1, undefined]);
+    const input: string[] = [];
+    const actual = zeroOneSequence.perform(input, 4);
+    const expected = ["0", "blank", "1", "blank", "blank"];
+    expect(actual).toEqual(expected);
   });
 
   it("6 sequence", () => {
-    const actual = turingMachine("b", zeroOneSequency, [], 6);
-    expect(actual).toEqual([0, undefined, 1, undefined, 0, undefined]);
+    const input: string[] = [];
+    const actual = zeroOneSequence.perform(input, 6);
+    const expected = ["0", "blank", "1", "blank", "0", "blank", "blank"];
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("Increment unary number", () => {
+  const INCREMENT_UNARY_NUMBER = new TuringMachine("q0", [
+    {
+      matchState: "q0",
+      matchSymbol: "1",
+      print: "none",
+      motion: 1,
+      state: "q0",
+    },
+    {
+      matchState: "q0",
+      matchSymbol: "blank",
+      print: "1",
+      motion: 0,
+      state: "finish",
+    },
+  ]);
+
+  it("0 -> 1", () => {
+    const actual = INCREMENT_UNARY_NUMBER.perform([]);
+    expect(actual).toEqual(["1"]);
+  });
+
+  it("3 -> 4", () => {
+    const input = ["1", "1", "1"];
+    const actual = INCREMENT_UNARY_NUMBER.perform(input);
+    const expected = ["1", "1", "1", "1"];
+    expect(actual).toEqual(expected);
   });
 });
