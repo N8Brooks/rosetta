658a031c0837d7b53eac5b481b1b70e6bd4388c7
skookumchoocher <natebrooks004@gmail.com>
2023-11-05 15:44:07 -0600

    feat(typescript): use relative file path

--- typescript/test_deps.ts
+++ typescript/test_deps.ts
@@ -1,8 +1,16 @@
+import * as Toml from "https://deno.land/std@0.109.0/encoding/toml.ts";
+import {
+  dirname,
+  fromFileUrl,
+  join,
+} from "https://deno.land/std@0.109.0/path/mod.ts";
+
 export * from "https://deno.land/std@0.205.0/assert/mod.ts";
 export * from "https://deno.land/std@0.205.0/testing/bdd.ts";
-import * as Toml from "https://deno.land/std@0.109.0/encoding/toml.ts";
 
 export async function readTestCases(name: string) {
-  const file = await Deno.readTextFile(`../testdata/${name}/test_cases.toml`);
+  const currentDir = dirname(fromFileUrl(import.meta.url));
+  const filePath = join(currentDir, "..", "testdata", name, "test_cases.toml");
+  const file = await Deno.readTextFile(filePath);
   return Toml.parse(file);
 }
